e3c7523d8d5b6bf0e2775b5187ed0694056a6e07
🙃leungyukshing <jacky14.liang@gmail.com>
2023-05-27 22:43:46 -0400

    feat: finish phase2 detection with timer

--- src/main/java/spendreport/FraudDetector.java
+++ src/main/java/spendreport/FraudDetector.java
@@ -24,7 +24,6 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
-import org.apache.flink.util.ConfigurationException;
 import org.apache.flink.walkthrough.common.entity.Alert;
 import org.apache.flink.walkthrough.common.entity.Transaction;
 
@@ -40,6 +39,7 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 	private static final long ONE_MINUTE = 60 * 1000;
 
 	private transient ValueState<Boolean> flagState;
+	private transient ValueState<Long> timerState;
 
 	@Override
 	public void processElement(
@@ -59,12 +59,17 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 			}
 
 			// Clean up our state
-			flagState.clear();
+			clearUp(context);
 		}
 
 		if (transaction.getAmount() < SMALL_AMOUNT) {
 			// set teh flag to true
 			flagState.update(true);
+
+			// set the timer and timer state
+			long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+			context.timerService().registerProcessingTimeTimer(timer);
+			timerState.update(timer);
 		}
 	}
 
@@ -75,5 +80,28 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 				Types.BOOLEAN
 		);
 		flagState = getRuntimeContext().getState(flagDescriptor);
+
+		ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<Long>(
+				"timer-state",
+				Types.LONG
+		);
+		timerState = getRuntimeContext().getState(timerDescriptor);
+	}
+
+	@Override
+	public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+		// remove flag after 1 minute
+		timerState.clear();
+		flagState.clear();
+	}
+
+	private void clearUp(Context ctx) throws Exception {
+		// delete timer
+		Long timer = timerState.value();
+		ctx.timerService().deleteProcessingTimeTimer(timer);
+
+		// clear up all state
+		timerState.clear();
+		flagState.clear();
 	}
 }
